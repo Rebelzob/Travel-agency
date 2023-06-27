f11d7dbdc5d5fffb4a6650b498372dc84fc15291
Rebelzob <karimdavid07@gmail.com>
2023-06-27 20:53:32 +0100

    added HTML and CSS of mobil version

---
+++ src/JS/cards.js
@@ -0,0 +1,33 @@
+const testimonials = [
+    {
+      name: "Sarah Johnson",
+      package: "Island Tours",
+      testimonial: "I had an amazing time exploring the Caribbean with Island Tours. Our guide was knowledgeable and friendly, and we saw some of the most beautiful sights I've ever seen. I highly recommend this tour!"
+    },
+    {
+      name: "John Smith",
+      package: "Beach Packages",
+      testimonial: "The Beach Package I booked with this travel agency was everything I hoped for and more. The beaches were pristine and the water was crystal clear. I can't wait to book another trip!"
+    },
+    {
+      name: "Maria Rodriguez",
+      package: "Flight Bookings",
+      testimonial: "Booking my flights to the Caribbean with this travel agency was a breeze. The staff was friendly and helpful, and I got a great deal on my tickets. I'll definitely book with them again!"
+    },
+    {
+      name: "David Lee",
+      package: "Hotel Reservations",
+      testimonial: "I stayed at a wonderful resort in the Caribbean that this travel agency recommended. The staff was friendly, the amenities were great, and the views were breathtaking. I can't wait to go back!"
+    },
+    {
+      name: "Emily Scott",
+      package: "Cruise Packages",
+      testimonial: "I had an incredible time on my Caribbean cruise thanks to this travel agency. The ship was beautiful, the food was delicious, and the ports of call were amazing. I highly recommend this cruise package!"
+    },
+    {
+      name: "Michael Brown",
+      package: "Island Adventures",
+      testimonial: "Island Adventures was the highlight of my Caribbean trip. We went snorkeling, hiking, and even visited a local market. Our guide was fantastic and made the experience unforgettable. I can't wait to go back and explore more islands with this agency!"
+    }
+  ];
+  
